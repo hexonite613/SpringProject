c827d9c1cb7b5b72cc3d757ae2c35e343b146ce5
성주원 <sungtv2613@naver.com>
2024-05-14 20:48:00 +0900

    Initial commit

---
+++ src/main/java/SpringProject/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package SpringProject.repository;
+
+import SpringProject.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article,Long> {
+}
---
+++ src/main/java/SpringProject/service/BlogService.java
@@ -0,0 +1,18 @@
+package SpringProject.service;
+
+import SpringProject.domain.Article;
+import SpringProject.dto.AddArticleRequest;
+import SpringProject.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+}
