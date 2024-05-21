68f2e9b6f9d0fafc798cad640be7751cab7ff1b4
성주원 <sungtv2613@naver.com>
2024-05-21 22:18:01 +0900

    delete completed

--- src/main/java/SpringProject/SpringProjectApplication.java
+++ src/main/java/SpringProject/SpringProjectApplication.java
@@ -2,7 +2,9 @@ package SpringProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringProjectApplication {
     public static void main(String[] args) {
