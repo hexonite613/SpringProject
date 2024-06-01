300cda507673fea63a385b8e025afd7e19879a98
성주원 <sungtv2613@naver.com>
2024-06-01 23:29:49 +0900

    signup, login back update

---
+++ src/main/java/SpringProject/controller/UserApiController.java
@@ -0,0 +1,20 @@
+package SpringProject.controller;
+
+import SpringProject.dto.AddUserRequest;
+import SpringProject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request){
+        userService.save(request);
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/SpringProject/service/UserService.java
@@ -0,0 +1,22 @@
+package SpringProject.service;
+
+import SpringProject.domain.User;
+import SpringProject.dto.AddUserRequest;
+import SpringProject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
