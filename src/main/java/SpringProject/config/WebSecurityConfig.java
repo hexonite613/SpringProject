0d8dbc37ec2532935d870abbfffd104d519652f7
성주원 <sungtv2613@naver.com>
2024-06-03 23:10:30 +0900

    signup, login completed

--- src/main/java/SpringProject/config/WebSecurityConfig.java
+++ src/main/java/SpringProject/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
                 .authorizeRequests()
-                .requestMatchers("/login","/signup","user").permitAll()
+                .requestMatchers("/login","/signup","/user").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
