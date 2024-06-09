8d65197c0fcfb1fa05f73b4325bc05f5e4396117
성주원 <sungtv2613@naver.com>
2024-06-09 22:20:37 +0900

    Authentication completed/tested

---
+++ src/main/java/SpringProject/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package SpringProject.service;
+
+import SpringProject.domain.RefreshToken;
+import SpringProject.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken){
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(()->new IllegalArgumentException("unexpected token"));
+    }
+}
---
+++ src/main/java/SpringProject/service/TokenService.java
@@ -0,0 +1,25 @@
+package SpringProject.service;
+
+import SpringProject.config.jwt.TokenProvider;
+import SpringProject.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken){
+        if(!tokenProvider.validToken(refreshToken)){
+            throw new IllegalStateException("unexpected token");
+        }
+        Long userId=refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user= userService.findById(userId);
+        return tokenProvider.generateToken(user, Duration.ofHours(2));
+    }
+}
